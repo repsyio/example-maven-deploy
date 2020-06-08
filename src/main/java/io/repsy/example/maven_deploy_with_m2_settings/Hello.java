5af9bb1e644d8c6abc76273ee7f1cb7bdf6c01e8
firatkucuk <firat@fria.io>
2020-06-08 21:16:38 +0200

    initial commit

---
+++ src/main/java/io/repsy/example/maven_deploy_with_m2_settings/Hello.java
@@ -0,0 +1,9 @@
+package io.repsy.example.maven_deploy_with_m2_settings;
+
+public class Hello {
+
+    public static String sayHello(final String name) {
+
+        return "Hello " + name + ", Greetings from repsy.io";
+    }
+}
